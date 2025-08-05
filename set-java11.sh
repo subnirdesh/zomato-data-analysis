a5c609da192f50a0157f005b3eaef26e8cdd3390
nirdesh <nirdesh.academics@gmail.com>
2025-08-05 07:34:32 +0545

    Apply .gitignore to already tracked files

--- set-java11.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home
-export PATH="/opt/homebrew/opt/openjdk@11/bin:$PATH"
-echo "Java environment set to:"
-java -version
